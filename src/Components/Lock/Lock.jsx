4585a1aec5ad0caa8f9259519f374d5e4cbe5d43
pawarit <67030314@kmitl.ac.th>
2025-03-25 19:51:34 +0700

    login

--- src/Components/Lock/Lock.jsx
+++ src/Components/Lock/Lock.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Lock.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-export const Lock = () => {
+const Lock = () => {
   return (
-    <div>Lock</div>
-  )
+    <div className='Login'>
+      <i class="ri-user-fill"></i>
+      <p className='test'>Username</p>
+      <input type='text' id='username' name='username' className='input'/>
+      <p className='test'>Password</p>
+      <input type='text' id='Password' name='Password'  className='input'/>
+    </div>       
+  );
 }
+
+export default Lock
